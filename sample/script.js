3989c0d65a2b40dc41619b2e83fe8c6eb3109286
trung.hoang <trung.hoang@sotatek.com>
2022-11-30 18:59:10 +0700

    bomber

--- sample/script.js
+++ sample/script.js
@@ -2,11 +2,11 @@ import http from "k6/http";
 import { check, group, sleep, fail } from "k6";
 
 export const options = {
-  vus: 1, // 1 user looping for 1 minute
-  duration: "1m",
+  vus: 10, // 1 user looping for 1 minute
+  duration: "10s",
 
   thresholds: {
-    http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
+    http_req_duration: ["p(99)<700"], // 99% of requests must complete below 1.5s
   },
 };
 
